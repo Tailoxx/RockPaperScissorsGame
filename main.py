5caf7e081459a2f01e815164fc109c38ef6b4a84
Tailoxx <terrak3232@gmail.com>
2026-02-24 14:31:31 +0300

    First Game

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config.config import Config, load_config
+from handlers.other import other_router
+from handlers.user import user_router
+
+logger = logging.getLogger(__name__)
+
+async def main():
+    config : Config = load_config()
+    logging.basicConfig(level=logging.getLevelName(config.log.level), format=config.log.format)
+    logger.info("Starting Bot")
+
+    bot = Bot(token= config.bot.token,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    dp.include_routers(user_router,other_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
